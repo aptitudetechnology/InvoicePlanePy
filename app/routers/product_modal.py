232ffb95057b6d61de2003db063252992a9ee0e5
Christopher Caston <chris@caston.id.au>
2025-07-19 16:47:53 +0800

    updated route

--- app/routers/product_modal.py
+++ app/routers/product_modal.py
@@ -15,7 +15,8 @@ from fastapi.templating import Jinja2Templates
 router = APIRouter()
 templates = Jinja2Templates(directory="app/templates")
 
-@router.get("/modals/add_product_modal", response_class=HTMLResponse)
+#@router.get("/modals/add_product_modal", response_class=HTMLResponse)
+@router.get("/add_product_modal", response_class=HTMLResponse)
 async def add_product_modal(request: Request, db: Session = Depends(get_db)):
     # Optionally fetch products from DB to pass to template
     products = db.query(Product).filter(Product.is_active == True).all()
