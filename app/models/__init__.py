3c643f9505dfe2e70e92999c2ae5c796d943367f
Christopher Caston <chris@caston.id.au>
2025-07-23 13:14:12 +0800

    updated check_db_schema script

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,3 +1,4 @@
+
 from .base import BaseModel
 from .user import User
 from .client import Client
@@ -5,7 +6,9 @@ from .invoice import Invoice, InvoiceItem, InvoiceStatus
 from .product import Product, ProductFamily, ProductUnit
 from .payment import Payment
 from .api_key import ApiKey
-from .quotes import Quote, QuoteStatus  # Change from Quotes to Quote
+from .quotes import Quote, QuoteStatus, QuoteItem
+from .tax_rate import TaxRate
+from .invoicesettings import InvoiceSettings
 
 __all__ = [
     "BaseModel",
